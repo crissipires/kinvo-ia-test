e196d064e7fc02d9a4e09baa1691e1d3f9bcbbd2
crissipires <crissiano_pire@hotmail.com>
2021-08-22 17:52:09 -0300

    :construction: testando scripts

--- space.py
+++ space.py
@@ -5,20 +5,21 @@ import json
 nlp = spacy.load("pt_core_news_sm")
 
 #função pra obter a entidade
-def getEntity():
-    with open('quotes.json','r') as file:
-        news = json.loads(file.read())
-        output = {}
 
-        for new in news:
-            new = new['news']
-            doc = nlp(new)
-            entidades = []
-            for entity in doc.ents:
-                entidades.append({
-                    "Trexo":entity.text,
-                    "Entidade":entity.label_
-                    })
+with open('news.json','r') as file:
+    news = json.loads(file.read())
+    output = {}
+
+    for new in news:
+        new = str(new)
+        doc = nlp(new)
+        entidades = []
+        for entity in doc.ents:
+            entidades.append({
+                "Trexo":entity.text,
+                "Entidade":entity.label_
+            })
             output[new] = entidades
-    return output
 
+
+print(output)
