d4c26d48b0aadbcb210913388cc99d11f238ad3e
crissipires <crissiano_pire@hotmail.com>
2021-08-22 17:51:14 -0300

    :heavy_plus_sign: criando endpoints

--- app.py
+++ app.py
@@ -1,14 +1,50 @@
-from flask import Flask
-import subprocess
+from flask import Flask,render_template
+import pandas as pd 
+import json
+import os
+import spacy
+
+
 app = Flask("Minerando")
 
-@app.route("/")
-def home():
-    spider_name = "news"
-    subprocess.check_output(['scrapy', 'crawl', spider_name, "-o", "news.json"])
-    with open("news.json") as items_file:
-        return items_file.read()
+@app.route('/getnews')
+def getnews():
+    
+    if not os.path.exists('news.json'):
+        os.system("scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 spiders/spiders/spiders/Myspider.py -o news.json")
+        
 
+    if os.path.exists('news.json'):
+        data = []
+        arq = pd.read_json('news.json',encoding='utf8')
+
+        for a in arq.values:
+            data.append(a)
+
+        return render_template('index.html',data=data)  
+
+
+@app.route('/getentity')
+def getentity():
+    #carrego o modelo e indentifico qual é a linguagem
+    nlp = spacy.load("pt_core_news_sm")
+
+    with open('news.json','r') as file:
+        news = json.loads(file.read())
+        output = {}
+
+        for new in news:
+            #lançando em string 
+            new = str(new)
+            doc = nlp(new)
+            entidades = []
+            for entity in doc.ents:
+                entidades.append({
+                    "Trexo":entity.text,
+                    "Entidade":entity.label_
+                    })
+            output[new] = entidades
+    return output
 
 
 if __name__ == '__main__':
